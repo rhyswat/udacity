89712b7debd504644e02359865c08e4a2a6ae5e4
rhyswat <rhys.watkin@icloud.com>
2014-02-11 21:29:03 +0000

    Policy computation by dynamic programming -- INCOMPLETE

---
+++ robotics_ai/policy_left_turn.py
@@ -0,0 +1,109 @@
+# ----------
+# User Instructions:
+# 
+# Implement the function optimum_policy2D() below.
+#
+# You are given a car in a grid with initial state
+# init = [x-position, y-position, orientation]
+# where x/y-position is its position in a given
+# grid and orientation is 0-3 corresponding to 'up',
+# 'left', 'down' or 'right'.
+#
+# Your task is to compute and return the car's optimal
+# path to the position specified in `goal'; where
+# the costs for each motion are as defined in `cost'.
+
+# EXAMPLE INPUT:
+
+# grid format:
+#     0 = navigable space
+#     1 = occupied space 
+grid = [[1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 0, 1, 1]]
+goal = [2, 0] # final position
+init = [4, 3, 0] # first 2 elements are coordinates, third is direction
+cost = [2, 1, 20] # the cost field has 3 values: right turn, no turn, left turn
+
+# EXAMPLE OUTPUT:
+# calling optimum_policy2D() should return the array
+# 
+# [[' ', ' ', ' ', 'R', '#', 'R'],
+#  [' ', ' ', ' ', '#', ' ', '#'],
+#  ['*', '#', '#', '#', '#', 'R'],
+#  [' ', ' ', ' ', '#', ' ', ' '],
+#  [' ', ' ', ' ', '#', ' ', ' ']]
+#
+# ----------
+
+
+# there are four motion directions: up/left/down/right
+# increasing the index in this array corresponds to
+# a left turn. Decreasing is is a right turn.
+
+forward = [[-1,  0], # go up
+           [ 0, -1], # go left
+           [ 1,  0], # go down
+           [ 0,  1]] # do right
+forward_name = ['up', 'left', 'down', 'right']
+
+# the cost field has 3 values: right turn, no turn, left turn
+action = [-1, 0, 1]
+action_name = ['R', '#', 'L']
+
+
+# ----------------------------------------
+# modify code below
+# ----------------------------------------
+
+def make_grid(v) :
+    return [[[v for row in range(len(grid[0]))] for col in range(len(grid))],
+            [[v for row in range(len(grid[0]))] for col in range(len(grid))],
+            [[v for row in range(len(grid[0]))] for col in range(len(grid))]]
+
+def optimum_policy2D():
+    value = make_grid(999)
+    policy = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
+    change = True
+
+    while change:
+        change = False
+
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if goal[0] == x and goal[1] == y :
+                    if value[x][y][t] > 0:
+                        value[x][y][t] = 0
+                        policy[x][y] = '*'
+                        change = True
+
+                elif grid[x][y] == 0:
+                    # perform each of the three actions
+                    orientation = .....
+                    for a in range(len(action)) :
+                        delta = 
+
+
+                    
+                    for a in range(len(delta)):
+                        x2 = x + delta[a][0]
+                        y2 = y + delta[a][1]
+
+                        if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
+                            v2 = value[x2][y2] + cost_step
+
+                            if v2 < value[x][y]:
+                                change = True
+                                value[x][y] = v2
+                                policy[x][y] = delta_name[a]
+
+    return policy # Make sure your function returns the expected grid.
+
+
+p = optimum_policy2D()
+for row in p :
+    print row
+
+
