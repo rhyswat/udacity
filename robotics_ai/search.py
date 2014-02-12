0cf19850c12480392b0535fadb480535a6de595e
Rhys Watkin <rhys.watkin@tekever.com>
2014-02-12 10:51:41 +0000

    Tidied, uses heapq

--- robotics_ai/search.py
+++ robotics_ai/search.py
@@ -14,21 +14,20 @@
 # You do not need to modify the heuristic.
 # ----------
 
+# grid is indexed [x][y] oddly
 grid = [[0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0]]
+x_max = len(grid) - 1
+y_max = len(grid[0]) - 1
 
-heuristic = [[9, 8, 7, 6, 5, 4],
-            [8, 7, 6, 5, 4, 3],
-            [7, 6, 5, 4, 3, 2],
-            [6, 5, 4, 3, 2, 1],
-            [5, 4, 3, 2, 1, 0]]
-
+# initial and goal state
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 
+# robot actions, assumed to always complete successfully
 delta = [[-1, 0 ], # go up
          [ 0, -1], # go left
          [ 1, 0 ], # go down
@@ -39,62 +38,19 @@ delta_name = ['^', '<', 'v', '>']
 cost = 1
 
 # ----------------------------------------
-# modify code below
+# helpers
 # ----------------------------------------
-
-def search():
-    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
-    closed[init[0]][init[1]] = 1
-
-    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
-    
-    # action list: {node:(parent, action taken at 'node')}
-    actions = {tuple(init): (None, None)}
-
-    x = init[0]
-    y = init[1]
-    g = 0
-
-    def heuristic_fn(x, y) :
-        return heuristic[x][y]
-
-    def h2(x,y) :
-        return abs(x - goal[0]) + abs(y - goal[1])
-
-    open_list = [[g + h2(x, y), g, x, y]]
-
-    found = False  # flag that is set when search is complete
-    resign = False # flag set if we can't find expand
-    count = 0
-    
-    while not found and not resign:
-        if len(open_list) == 0:
-            resign = True
-            return "Fail"
-        else:
-            open_list.sort()
-            open_list.reverse()
-            current = open_list.pop()
-            f, g, x, y = current
-            expand[x][y] = count
-            count += 1
-            
-            if x == goal[0] and y == goal[1]:
-                found = True
-            else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            open_list.append([g2 + heuristic_fn(x2, y2), g2, x2, y2])
-                            closed[x2][y2] = 1
-                            actions[(x2, y2)] = ((x,y), i)
+def print_expansions(expand) :
+    print ''
+    print 'Expansions table'
     for i in range(len(expand)):
-        print expand[i]
-        
-    # make the funky path depiction
+        print ' '.join(['  %2d' %j for j in expand[i]])
+    
+def print_grid() :
+    for g in grid :
+        print g
+
+def build_path(actions) :
     node, action = tuple(goal), None
     path = []
     while node is not None and actions.has_key(node) :
@@ -103,21 +59,109 @@ def search():
         node, action = actions[node]
     path.reverse()
     print ''
-    print 'Path:'
-    print " -> ".join([str(z) for z in path])
+    print 'Path: ({} steps)'.format(len(path)-1) # -1 as the path is the list of nodes not steps
+    if len(path) > 0 :
+        print ' -> '.join([str(n) for n,a in path])
+    return path
 
-    visual_path = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
-    for x in range(len(grid)) :
-        for y in range(len(grid[0])) :
-            if grid[x][y] == 1 :
-                visual_path[x][y] = '*' # obstacle
+def print_route(path) :
+    print ''
+    print 'Path taken:'
+    visual_path = [[' ' for row in range(len(grid[0])+2)] for col in range(len(grid)+2)]
+    for x in range(len(visual_path)) :
+        for y in range(len(visual_path[0])) :
+            if (x == 0 and y == 0) \
+               or (x == 0 and y == y_max+2) \
+               or (x== x_max+2 and y == 0) \
+               or (x == x_max+2 and y == y_max+2) :
+                visual_path[x][y] = '+'
+            elif x == 0 or x == x_max+2 :
+                visual_path[x][y] = '-'
+            elif y==0 or y == y_max+2 :
+                visual_path[x][y] = '|'
+            elif 1<=x and x <=x_max+1 and 1<=y and y<=y_max+1 and grid[x-1][y-1] == 1:
+                visual_path[x][y] = '*'
     for n,a in path :
         x,y = n
         s = a is None and 'G' or delta_name[a]
-        visual_path[x][y] = s
-    visual_path[init[0]][init[1]] = 'S'
+        visual_path[x+1][y+1] = s
+    visual_path[init[0]+1][init[1]+1] = 'S'
     for v in visual_path :
-        print v
+        print ' '.join(v)    
+
+# ----------------------------------------
+# a*-search
+# ----------------------------------------
+import heapq
+def search():
+    # ----------------------------------------------------------------------
+    # expand[x][y] is the step at which xy was visited
+    # -1 means never visited (which might be a good thing)
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    count = 0
+
+    # ----------------------------------------------------------------------
+    # visited nodes
+    marked = set()
+
+    # ----------------------------------------------------------------------
+    # action list: {node:(parent, action taken at 'node')}
+    actions = {tuple(init): (None, None)}
+
+    # ----------------------------------------------------------------------
+    # the heuristic function    
+    def heuristic(x,y) :
+        return abs(x - goal[0]) + abs(y - goal[1])
+
+    # ----------------------------------------------------------------------
+    xx, yy = init
+    open_list = [(heuristic(xx, yy), 0, xx, yy)]
+    heapq.heapify(open_list)
+    found = False
+    while len(open_list) > 0:
+        f, g, x, y = heapq.heappop(open_list)
+
+        # are we at the goal?
+        if x == goal[0] and y == goal[1] :
+            expand[x][y] = count
+            count += 1
+            found = True
+            break
+
+        # this location is on the path if it's not visited
+        location = (x,y)
+        if location not in marked :
+            expand[x][y] = count
+            count += 1
+            marked.add(location)
+            for i in xrange(len(delta)) :
+                d = delta[i]
+                p = (x + d[0], y + d[1])
+                if p not in marked \
+                   and 0 <= p[0] and p[0] <= x_max \
+                   and 0 <= p[1] and p[1] <= y_max \
+                   and grid[p[0]][p[1]] != 1 :
+                    g2 = g + cost
+                    heapq.heappush(open_list, (g2 + heuristic(*p), g2, p[0], p[1]))
+                    actions[p] = ((x,y), i)
+
+    if not found :
+        print '** FAILED **'
+        print '   No route from {} to {} in this grid'.format(init, goal)
+        print_grid()
+        return 'fail'
+    
+    # -- visualisation -------------------------------------------------
+    # otherwise...
+    # show the expansions
+    print_expansions(expand)
+        
+    # make the funky path depiction
+    path = build_path(actions)
+
+    # print a visual grid with a bounding edge
+    print_route(path)
+    # ----------------------------------------------------------------------
     
         
     return expand #Leave this line for grading purposes!
